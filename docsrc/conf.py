ac8120888332d4052dfe3ef61a9edb2ee0361772
Johan Hidding <j.hidding@esciencecenter.nl>
2022-12-07 13:49:20 +0100

    rebuild documentation

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -68,7 +68,7 @@ release = byteparsing.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -88,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
